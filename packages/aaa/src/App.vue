4f0ffdddee1bc82eb067950f19e204406026ae8d
babahironobu <ghdtjs0417@gmail.com>
2021-11-06 23:50:01 +0900

    init

---
+++ packages/aaa/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+</template>
+
+<script lang="ts">
+import { defineComponent, onMounted } from 'vue';
+import { hello } from '@baba/bbb';
+import HelloWorld from './components/HelloWorld.vue';
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  setup() {
+    onMounted(() => {
+      hello();
+    });
+  },
+});
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
