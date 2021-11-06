294ccfcfe0a6c25960304c278e8b09300fce3b62
babahironobu <ghdtjs0417@gmail.com>
2021-11-07 00:03:19 +0900

    Modify: 문구 변경

--- packages/aaa/src/App.vue
+++ packages/aaa/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <HelloWorld msg="어서오세요!"/>
 </template>
 
 <script lang="ts">
